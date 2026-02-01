e6a310eec52bc5af3bf72b05596d8dee4d66be30
Soveer P Shah <soveer18@gmail.com>
2026-02-01 18:20:45 +0530

    removed use from import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {use, useState} from "react";
+import {useState} from "react";
 import {getWeather} from "./services/weatherApi";
 import WeatherCard from "./components/WeatherCard";
 import "./App.css"
